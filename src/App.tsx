0afd41ef2e1807f98f8ef51928f083cc03140c5e
Jose Manuel <jmdcarq89@gmail.com>
2021-02-24 22:37:48 +0100

    App return

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import {useState} from 'react'
 import {useQuery} from 'react-query'
 
+import Item from './Item/Item';
+import Cart from './Cart/Cart';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 // Styles
-import {Wrapper} from './App.styles';
+import {Wrapper, StyledButton} from './App.styles';
 // Types
 export type CartItemType = {
   id: number;
@@ -25,13 +27,15 @@ const getProducts = async (): Promise<CartItemType[]> =>
 
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error} = useQuery<CartItemType[]>(
     'products',
     getProducts
   );
   console.log(data);
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => null;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -41,7 +45,29 @@ const App = () => {
   if (error) return <div>Something went wrong...</div>;
 
 
-  return <div className="App">Start</div>;
+  return (
+    <Wrapper>
+        <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+        <Grid container spacing={3}>
+            {data?.map(item => (
+              <Grid item key={item.id} xs={12} sm={4}>
+                  <Item item={item} handleAddToCart={handleAddToCart} />
+              </Grid>
+            ))}
+        </Grid>
+    </Wrapper>
+  )
 };
 
 export default App;
